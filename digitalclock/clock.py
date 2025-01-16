988a63a1fb1f411e31ecfd8422d93488cc0a8b3c
yashika yadav <yashikarao22j@gmail.com>
2025-01-16 10:32:17 +0530

    digital clock

---
+++ digitalclock/clock.py
@@ -0,0 +1,14 @@
+#digital clock using python
+import tkinter as tk
+from time import strftime
+root = tk.Tk()
+root.title("DIGITAL CLOCK")
+#define function
+def time():
+    string = strftime('%H:%M:%S %p \n %D')
+    label.config(text=string)
+    label.after(1000,time)
+label=tk.Label(root, font=('calibri', 70, 'bold'), background='pink', foreground='black')
+label.pack(anchor='center')
+time()
+root.mainloop()
